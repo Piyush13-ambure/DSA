51355247cfb81752f69cbdc54143902311184232
Piyush <amburepiyush@gmail.com>
2025-05-03 11:54:52 +0530

    Create Chocolates Pickup.cpp

---
+++ Chocolates Pickup.cpp
@@ -0,0 +1,78 @@
+/*You are given an n rows and m cols matrix grid representing a field of chocolates where grid[i][j] represents the number of chocolates that you can collect from the (i, j) cell.
+
+You have two robots that can collect chocolates for you:
+
+Robot #1 is located at the top-left corner (0, 0), and
+Robot #2 is located at the top-right corner (0, cols - 1).
+Return the maximum number of chocolates collection using both robots by following the rules below:
+
+From a cell (i, j), robots can move to cell (i + 1, j - 1), (i + 1, j), or (i + 1, j + 1).
+When any robot passes through a cell, It picks up all chocolates, and the cell becomes an empty cell.
+When both robots stay in the same cell, only one takes the chocolates.
+Both robots cannot move outside of the grid at any moment.
+Both robots should reach the bottom row in grid.
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int solve(int n, int m, vector<vector<int>>& grid) {
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(m, -1)));
+
+        // Base case: last row
+        for (int j1 = 0; j1 < m; j1++) {
+            for (int j2 = 0; j2 < m; j2++) {
+                if (j1 == j2)
+                    dp[n - 1][j1][j2] = grid[n - 1][j1];
+                else
+                    dp[n - 1][j1][j2] = grid[n - 1][j1] + grid[n - 1][j2];
+            }
+        }
+
+        // Fill dp from bottom to top
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j1 = 0; j1 < m; j1++) {
+                for (int j2 = 0; j2 < m; j2++) {
+                    int maxi = 0;
+                    for (int dj1 = -1; dj1 <= 1; dj1++) {
+                        for (int dj2 = -1; dj2 <= 1; dj2++) {
+                            int nj1 = j1 + dj1;
+                            int nj2 = j2 + dj2;
+                            if (nj1 >= 0 && nj1 < m && nj2 >= 0 && nj2 < m) {
+                                int val = 0;
+                                if (j1 == j2)
+                                    val = grid[i][j1];
+                                else
+                                    val = grid[i][j1] + grid[i][j2];
+                                val += dp[i + 1][nj1][nj2];
+                                maxi = max(maxi, val);
+                            }
+                        }
+                    }
+                    dp[i][j1][j2] = maxi;
+                }
+            }
+        }
+
+        return dp[0][0][m - 1];
+    }
+};
+
+int main() {
+    int n = 4, m = 3;
+    vector<vector<int>> grid = {
+        {3, 1, 1},
+        {2, 5, 1},
+        {1, 5, 5},
+        {2, 1, 1}
+    };
+
+    Solution sol;
+    cout << "Maximum chocolates collected: " << sol.solve(n, m, grid) << endl;
+
+    return 0;
+}
