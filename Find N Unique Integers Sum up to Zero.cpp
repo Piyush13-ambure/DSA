53e887c36db3b2b5345e8298ca5f91b5d0c2b36a
Piyush <amburepiyush@gmail.com>
2025-09-07 22:27:20 +0530

    Create  Find N Unique Integers Sum up to Zero.cpp

---
+++ Find N Unique Integers Sum up to Zero.cpp
@@ -0,0 +1,16 @@
+/*Given an integer n, return any array containing n unique integers such that they add up to 0.*/
+class Solution {
+public:
+    vector<int> sumZero(int n) {
+        vector<int> a;
+        if (n % 2 != 0) {
+            a.push_back(0);
+        }
+
+        for (int i = 1; i <= n/2; i++) {
+            a.push_back(i);
+            a.push_back(-i);
+        }
+        return a;
+    }
+};
