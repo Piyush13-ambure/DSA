0789dfade4e0e78ba21602357b719e2c14054085
Piyush <amburepiyush@gmail.com>
2025-10-22 19:47:54 +0530

    Create Reverse array in groups12.cpp

---
+++ Reverse array in groups12.cpp
@@ -0,0 +1,15 @@
+/*Given an array arr[] of positive integers. Reverse every sub-array group of size k.
+Note: If at any instance, k is greater or equal to the array size, then reverse the entire array. */
+
+class Solution {
+  public:
+    void reverseInGroups(vector<int>& arr, int k) {
+        int n = arr.size();
+        for (int i = 0; i < n; i += k) {
+            // Determine the end point for the current sub-array.
+            int end = min(i + k, n);
+            // Reverse the sub-array from arr[i] to arr[end - 1].
+            reverse(arr.begin() + i, arr.begin() + end);
+        }
+    }
+};
