36aca19fb59c0973044d18b059b3161c4b83c7ab
Piyush <amburepiyush@gmail.com>
2025-10-24 19:00:21 +0530

    Create Add 1 to a Linked List Number.cpp

---
+++ Add 1 to a Linked List Number.cpp
@@ -0,0 +1,53 @@
+/*You are given a linked list where each element in the list is a node and have an integer data. You need to add 1 to the number formed by concatinating all the list node numbers together and return the head of the modified linked list. 
+
+Note: The head represents the first element of the given array.
+
+*/
+
+class Solution {
+
+  public:
+    Node* reverse(Node* head) {
+        Node* prev = NULL;
+        Node* current = head;
+        Node* next;
+
+        while (current != NULL) {
+            next = current->next; // storing next node
+            current->next = prev; // linking current node to previous
+            prev = current;       // updating prev
+            current = next;       // updating current
+        }
+
+        return prev;
+    }
+
+  public:
+    Node* addOne(Node* head) {
+        head = reverse(head); // reversing list to make addition easy
+
+        Node* current = head;
+        int carry = 1;
+
+        while (carry) {
+            current->data += 1; // adding one to current node
+
+            if (current->data < 10)
+                return reverse(head);
+            // if no carry we can reverse back list and return it
+            else
+                current->data = 0;
+            // else we continue with taking carry forward
+
+            if (current->next == NULL)
+                break;
+            // if, end of list, we break from loop
+            else
+                current = current->next;
+            // else we move to next node
+        }
+
+        current->next = new Node(1); // adding new node for the carried 1
+        return reverse(head);
+    }
+};
