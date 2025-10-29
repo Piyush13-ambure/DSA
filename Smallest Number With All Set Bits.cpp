7a5293acbd78f7c89e3c0489bc232e5f00270981
Piyush <amburepiyush@gmail.com>
2025-10-29 22:37:52 +0530

    Create Smallest Number With All Set Bits.cpp

---
+++ Smallest Number With All Set Bits.cpp
@@ -0,0 +1,14 @@
+/*You are given a positive number n.
+
+Return the smallest number x greater than or equal to n, such that the binary representation of x contains only set bits
+
+*/
+class Solution {
+public:
+    int smallestNumber(int n) {
+        while (n & (n + 1)) {
+            n |= n + 1;
+        }
+        return n;
+    }
+};
